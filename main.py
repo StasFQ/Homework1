a9994337e7b8f6b3a147b593030b5c25360d9b59
Stas <stas@MacBook-Air-Stas.local>
2022-09-29 17:48:46 +0300

    realised function parser, start make tests

--- main.py
+++ main.py
@@ -1,21 +1,38 @@
-def parse(query: str) -> dict:
-    return {}
+from urllib import parse
+def parse_d(url) -> dict:
+    if url:
+        parse.urlsplit(url)
+        parse.parse_qs(parse.urlsplit(url).query)
+        a = dict(parse.parse_qsl(parse.urlsplit(url).query))
+        return a
+    else:
+        return {}
 
 
 if __name__ == '__main__':
-    assert parse('https://example.com/path/to/page?name=ferret&color=purple') == {'name': 'ferret', 'color': 'purple'}
-    assert parse('https://example.com/path/to/page?name=ferret&color=purple&') == {'name': 'ferret', 'color': 'purple'}
-    assert parse('http://example.com/') == {}
-    assert parse('http://example.com/?') == {}
-    assert parse('http://example.com/?name=Dima') == {'name': 'Dima'}
+    assert parse_d('https://example.com/path/to/page?name=ferret&color=purple') == {'name': 'ferret', 'color': 'purple'}
+    assert parse_d('https://example.com/path/to/page?name=ferret&color=purple&') == {'name': 'ferret', 'color': 'purple'}
+    assert parse_d('http://example.com/') == {}
+    assert parse_d('http://example.com/?') == {}
+    assert parse_d('http://example.com/?name=Dima') == {'name': 'Dima'}
+    assert parse_d('')
+    assert parse_d('')
+    assert parse_d('')
+    assert parse_d('')
+    assert parse_d('')
+    assert parse_d('')
+    assert parse_d('')
+    assert parse_d('')
+    assert parse_d('')
+    assert parse_d('')
 
 
-def parse_cookie(query: str) -> dict:
-    return {}
+#def parse_cookie(query: str) -> dict:
+   # return {}
 
 
-if __name__ == '__main__':
-    assert parse_cookie('name=Dima;') == {'name': 'Dima'}
-    assert parse_cookie('') == {}
-    assert parse_cookie('name=Dima;age=28;') == {'name': 'Dima', 'age': '28'}
-    assert parse_cookie('name=Dima=User;age=28;') == {'name': 'Dima=User', 'age': '28'}
+#if __name__ == '__main__':
+    #assert parse_cookie('name=Dima;') == {'name': 'Dima'}
+   # assert parse_cookie('') == {}
+    #assert parse_cookie('name=Dima;age=28;') == {'name': 'Dima', 'age': '28'}
+   #assert parse_cookie('name=Dima=User;age=28;') == {'name': 'Dima=User', 'age': '28'}
