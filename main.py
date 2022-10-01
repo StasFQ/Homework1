73cf5d1f8346bf200790f6ed7119d30dc7afb9e0
StasFQ <113999763+StasFQ@users.noreply.github.com>
2022-10-01 18:18:11 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
-#def parse(query: str) -> dict:
-    #return {}
+def parse(query: str) -> dict:
+    return {}
 
 
-#if __name__ == '__main__':
-    #assert parse('https://example.com/path/to/page?name=ferret&color=purple') == {'name': 'ferret', 'color': 'purple'}
-    #assert parse('https://example.com/path/to/page?name=ferret&color=purple&') == {'name': 'ferret', 'color': 'purple'}
-    ##assert parse('http://example.com/?') == {}
-   # assert parse('http://example.com/?name=Dima') == {'name': 'Dima'}
+if __name__ == '__main__':
+    assert parse('https://example.com/path/to/page?name=ferret&color=purple') == {'name': 'ferret', 'color': 'purple'}
+    assert parse('https://example.com/path/to/page?name=ferret&color=purple&') == {'name': 'ferret', 'color': 'purple'}
+    assert parse('http://example.com/?') == {}
+    assert parse('http://example.com/?name=Dima') == {'name': 'Dima'}
 
 
 def parse_cookie(cooks) -> dict:
